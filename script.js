6590e1ec2c48d1c1298c00134ff09753e4990267
student <you@example.com>
2023-06-19 19:37:47 +0300

    Edits on to do list

--- script.js
+++ script.js
@@ -1,39 +1,58 @@
-fetch('https://dummyjson.com/todos/user/5')
-  .then(res => res.json())
-  .then(data => console.log(data));
-  const checkboxes = document.querySelectorAll('input[type="checkbox"]');
-  checkboxes.forEach(checkbox => {
-    checkbox.addEventListener('change', () => {
-      const parentLi = checkbox.parentElement;
-      if (checkbox.checked) {
-        parentLi.classList.add('completed');
-      } else {
-        parentLi.classList.remove('completed');
-      }
-    });
+document.addEventListener("DOMContentLoaded", function () {
+	var newTaskInput = document.querySelector("#newtask input[type='text']");
+	var addButton = document.querySelector("#newtask button");
+	var tasksContainer = document.querySelector("#tasks");
+  
+	addButton.addEventListener("click", function () {
+	  var taskText = newTaskInput.value;
+	  if (taskText.trim() !== "") {
+		var taskElement = document.createElement("div");
+		taskElement.classList.add("task");
+		taskElement.innerHTML = `
+		  <span>${taskText}</span>
+		  <button class="delete">-</button>
+		  <button class="fue">...</button>
+		`;
+		tasksContainer.appendChild(taskElement);
+		newTaskInput.value = "";
+		
+	  }
+	});
+  
+	tasksContainer.addEventListener("click", function (event) {
+	   if (event.target.classList.contains("delete")) {
+		var taskElement = event.target.closest(".task");
+		taskElement.remove();
+	  } else if (event.target.classList.contains("fue")) {
+		var taskElement = event.target.closest(".task");
+		var spanElement = taskElement.querySelector("span");
+		var taskText = spanElement.textContent;
+		newTaskInput.value = taskText;
+		taskElement.remove();
+	  }
+	});
+  
+	var links = ['https://dummyjson.com/todos/6', 'https://dummyjson.com/todos/9', 'https://dummyjson.com/todos/8', 'https://dummyjson.com/todos/6', 'https://dummyjson.com/todos/10'];
+  
+	links.forEach(function (link) {
+	  fetch(link)
+		.then(function (response) {
+		  return response.json();
+		})
+		.then(function (data) {
+		  var taskText = data.title || data.todo;
+		  var taskElement = document.createElement("div");
+		  taskElement.classList.add("task");
+		  taskElement.innerHTML = `
+			<span>${taskText}</span>
+			<button class="delete">-</button>
+			<button class="update">...</button>
+		  `;
+		  tasksContainer.appendChild(taskElement);
+		})
+		.catch(function (error) {
+		  console.log("Error fetching data:", error);
+		});
+	});
   });
-  function addNewTask() {
-    
-  }
-  const addBtn = document.querySelector('button');
-const inputField = document.querySelector('input[type="text"]');
-const taskList = document.querySelector('#task');
-addBtn.addEventListener('click', () => {
-  const newTask = inputField.value;
-  const newLi = document.createElement('li');
-  newLi.innerHTML = `
-    <input type="checkbox">
-    <label>${newTask}</label>
-    <button class="complete-btn">Complete</button>
-    <button class="delete-btn">Delete</button>
-  `;
-  taskList.appendChild(newLi);
-  inputField.value = '';
-});
-const deleteBtns = document.querySelectorAll('.delete-btn');
-deleteBtns.forEach(deleteBtn => {
-  deleteBtn.addEventListener('click', () => {
-    const parentLi = deleteBtn.parentElement;
-    parentLi.remove();
-  });
-});
+  
